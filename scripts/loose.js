9122bc93d37f9dc83a83c10d308e16b1003c5221
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-03-06 17:32:41 +0100

    finished loose page

--- scripts/loose.js
+++ scripts/loose.js
@@ -2,11 +2,17 @@
 function loosepage() {
   console.log("loosepage");
   const form = document.getElementsByTagName("form");
-  const p = document.createElement("p");
+  const h3 = document.createElement("h3");
   const h2 = document.getElementsByTagName("h2");
+  const p = document.createElement("p");
+  const br = document.createElement("br");
 
+  form[0].appendChild(h3);
+  form[0].appendChild(br);
   form[0].appendChild(p);
 
   h2[0].textContent = "Du hast verloren!!!";
-  p.textContent = "";
+  h3.textContent = "Leider hast du nichts gewonnen:(";
+  p.textContent =
+    "Das Swiss Contingent wünscht dir trotzdem noch einen schönen 1. August";
 }
