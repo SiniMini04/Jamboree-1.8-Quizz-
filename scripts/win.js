ace4b8aceee8255bdf2cfcd4ad37205b988652d1
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-03-06 18:33:14 +0100

    implimented winpage and wincode generator

--- scripts/win.js
+++ scripts/win.js
@@ -1,1 +1,40 @@
-function winpage() {}
+//general variables
+const lengthwincode = 6;
+
+//Generate winpage
+function winpage() {
+  console.log("winpage");
+  const form = document.getElementsByTagName("form");
+  const h2 = document.getElementsByTagName("h2");
+  const h3 = document.createElement("h3");
+  const h4 = document.createElement("h4");
+  const p = document.createElement("p");
+  const br = document.createElement("br");
+
+  form[0].appendChild(h3);
+  form[0].appendChild(h4);
+  form[0].appendChild(br);
+  form[0].appendChild(p);
+
+  const wincode = generatewincode(lengthwincode);
+
+  h2[0].textContent = "Du hast gewonnen!!!";
+  h3.textContent = wincode;
+  h4.textContent =
+    "Mach einen Screenshot von diesem Code oder notiere ihn. Damit kannst du während des ganzen Jamborees einen kleinen Preis im schweizer Delegationszelt holen;)";
+  p.textContent =
+    "Das Swiss Contingent wünscht dir noch einen schönen 1. August";
+}
+
+//Generate wincode
+function generatewincode(n) {
+  let randomString = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for (let i = 0; i < n; i++) {
+    randomString += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return randomString;
+}
