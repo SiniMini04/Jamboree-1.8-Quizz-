bc7da79a70bcd8cf3e52f3a65f1cd823cf5a95cc
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-06-29 10:09:21 +0200

    added frontend in backend

--- QuizzApp/server/index.js
+++ QuizzApp/server/index.js
@@ -11,13 +11,81 @@ app.use(
 );
 app.use(express.json({ limit: "5mb" })); //sets data limit of parsing in body to 50mb //to manage the parsing of the body from react app
 app.use(express.urlencoded({ limit: "5mb" })); //sets data limit of parsing in url to 50mb
-app.use(express.static(path.join(__dirname, 'site')));
+app.use(express.static(path.join(__dirname, "site")));
+
 app.post("/writeToDb", (req, res) => {
   writeindb(req.body.code);
   res.send({
     code: req.body.code,
   });
 });
+
+app.get("/checkCode/:id", async (req, res) => {
+  let response = await checkcode(req.params.id);
+  console.log(response[0]);
+
+  if (response[0] == true) {
+    deletcode(response[1]);
+  }
+  res.send(response[0]);
+});
+
+async function checkcode(wincode) {
+  const { MongoClient } = require("mongodb");
+
+  const uri =
+    "mongodb+srv://SinanM:WU9G3Cat3Qt9qwcu@codes.10bnqin.mongodb.net/test";
+
+  const client = new MongoClient(uri);
+  try {
+    let answer = [];
+
+    await client.connect();
+
+    const database = client.db("Codes");
+    const collection = database.collection("Codes");
+
+    const query = { code: wincode };
+    const result = await collection.findOne(query);
+    console.log(result);
+
+    if (result) {
+      answer.push(true);
+      console.log(result._id);
+
+      answer.push(result._id);
+    } else {
+      answer.push(false);
+    }
+
+    return answer;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function deletcode(wincode) {
+  const { MongoClient } = require("mongodb");
+
+  const uri =
+    "mongodb+srv://SinanM:WU9G3Cat3Qt9qwcu@codes.10bnqin.mongodb.net/test";
+
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+
+    const database = client.db("Codes");
+    const collection = database.collection("Codes");
+
+    const query = { _id: wincode };
+    const result = await collection.deleteOne(query);
+    console.log(result);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 
 async function writeindb(wincode) {
   const { MongoClient } = require("mongodb");
